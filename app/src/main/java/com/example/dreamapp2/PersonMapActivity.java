22f8868b39fb9c454b7a2d72cfa04f4d52308212
sarascript-sensei <sara.2000year14@gmail.com>
2020-08-03 15:09:09 +0600

    ПОЛУЧИЛООООООООООООООООООООСЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/src/main/java/com/example/dreamapp2/PersonMapActivity.java
+++ app/src/main/java/com/example/dreamapp2/PersonMapActivity.java
@@ -6,10 +6,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.firebase.geofire.GeoFire;
@@ -24,10 +26,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -41,18 +45,42 @@ public class PersonMapActivity extends FragmentActivity implements OnMapReadyCal
     FloatingActionButton fab, fab1, fab2;
     FloatingActionMenu materialDesignFAM;
     GoogleApiClient googleApiClient;
+    private String personId;
     Location lastLocation;
     LocationRequest locationRequest;
+    private LatLng PersonPostion;
+
+    BitmapDescriptorFactory icon;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference PersonDatabaseRef;
+
+    private ImageButton LogOutPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
+        LogOutPerson = (ImageButton)findViewById(R.id.LogOutPerson);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        personId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        PersonDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Person's Request");
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        assert mapFragment != null;
         mapFragment.getMapAsync(this);
+
+        LogOutPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                LogOutPerson();
+            }
+        });
 
         materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
         fab = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
@@ -62,6 +90,18 @@ public class PersonMapActivity extends FragmentActivity implements OnMapReadyCal
         fab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(PersonMapActivity.this, "Маркер выбран", Toast.LENGTH_SHORT).show();
+                GeoFire geofire = new GeoFire(PersonDatabaseRef);
+                geofire.setLocation(personId, new GeoLocation(lastLocation.getLatitude(), lastLocation.getLongitude()));
+
+                PersonPostion = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.oxygen);
+
+                MarkerOptions markerOptions = new MarkerOptions().position(PersonPostion)
+                        .title("Current Location")
+                        .snippet("hello").icon(icon);
+
+
+                mMap.addMarker(markerOptions);
             }
         });
         fab1.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +116,8 @@ public class PersonMapActivity extends FragmentActivity implements OnMapReadyCal
             }
         });
     }
+
+
 
 
     @Override
@@ -135,6 +177,12 @@ public class PersonMapActivity extends FragmentActivity implements OnMapReadyCal
 
     @Override
     protected void onStop() {
+
         super.onStop();
+    }
+    private void LogOutPerson() {
+        Intent welcomeIntent = new Intent(PersonMapActivity.this, WelcomeActivity.class);
+        startActivity(welcomeIntent);
+        finish();
     }
 }
