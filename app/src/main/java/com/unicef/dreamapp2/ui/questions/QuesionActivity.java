0a6a77013abd9d6caff90096a18476982bf7341d
sarascript-sensei <sara.2000year14@gmail.com>
2020-09-07 12:38:00 +0600

    Questions and suggestions Activity

---
+++ app/src/main/java/com/unicef/dreamapp2/ui/questions/QuesionActivity.java
@@ -0,0 +1,13 @@
+package com.unicef.dreamapp2.ui.questions;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quesion);
+    }
+}
