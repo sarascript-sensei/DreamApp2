d73c43107f019dc917c6cbe3113c65ef2091a73b
sarascript-sensei <sara.2000year14@gmail.com>
2020-09-19 19:27:50 +0600

    Changed rating and added new page

---
+++ app/src/main/java/com/unicef/dreamapp2/PsychologyHelp.java
@@ -0,0 +1,14 @@
+package com.unicef.dreamapp2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PsychologyHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_psychology_help);
+    }
+}
